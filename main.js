c4c23e3a30d56f1a4cdb2b441ae44eace8c64d1a
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-03-09 09:23:38 +0100

    update

--- main.js
+++ main.js
@@ -16,22 +16,22 @@ const observer = new IntersectionObserver((entries) => {
 
 document.querySelectorAll('header, .image_affiche, .lien_google_maps, .image_de_la_semaine').forEach(el => observer.observe(el));
 
-const iconeDiscord = document.querySelector('.icone_discord')
-const iconeInstagram = document.querySelector('.icone_instagram')
+const iconeDiscord = document.querySelector('.icone_discord');
+const iconeInstagram = document.querySelector('.icone_instagram');
 
 iconeDiscord.addEventListener('mouseenter', ()=>{
     iconeDiscord.src = 'img/Discord_hover.png'
-})
+});
 
 iconeDiscord.addEventListener('mouseleave', ()=>{
     iconeDiscord.src = 'img/Discord.png'
-})
+});
 
 
 iconeInstagram.addEventListener('mouseenter', ()=>{
     iconeInstagram.src = 'img/Instagram_hover.png'
-})
+});
 
 iconeInstagram.addEventListener('mouseleave', ()=>{
     iconeInstagram.src = 'img/Instagram.png'
-})
+});
